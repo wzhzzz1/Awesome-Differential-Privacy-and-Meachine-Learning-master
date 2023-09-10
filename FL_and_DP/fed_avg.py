e0499289a8a231f620cd1042e6d7c4ddbe66f49f
wangzihang <1013054127@qq.com>
2023-09-10 11:13:15 +0800

    添加其他依赖模块的__init__.py

--- FL_and_DP/fed_avg.py
+++ FL_and_DP/fed_avg.py
@@ -1,11 +1,11 @@
 from fl_utils.center_average_model_with_weights import set_averaged_weights_as_main_model_weights, \
     set_averaged_weights_as_main_model_weights_fully_averaged
-#from fl_utils.local_clients_train_process import local_clients_train_process_without_dp_one_epoch, \
-#    local_clients_train_process_without_dp_one_batch
+from fl_utils.local_clients_train_process import local_clients_train_process_without_dp_one_epoch, \
+    local_clients_train_process_without_dp_one_batch
 from fl_utils.send_main_model_to_clients import send_main_model_to_clients
-from data.fed_data_distribution.dirichlet_nonIID_data import fed_dataset_NonIID_Dirichlet
+from fed_data_distribution.dirichlet_nonIID_data import fed_dataset_NonIID_Dirichlet
 from fl_utils.optimizier_and_model_distribution import create_model_optimizer_criterion_dict
-from data.fed_data_distribution.pathological_nonIID_data import pathological_split_noniid
+from fed_data_distribution.pathological_nonIID_data import pathological_split_noniid
 from data.get_data import get_data
 from model.CNN import CNN, Cifar10CNN
 from train_and_validation.validation import validation
