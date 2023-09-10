6a5f1cd6b3f09978cef87d5c5c45edb660589b69
wangzihang <1013054127@qq.com>
2023-09-10 11:44:47 +0800

    删除pylab的引用，因为在python新版本已经没了

--- FL_and_DP/fed_avg.py
+++ FL_and_DP/fed_avg.py
@@ -1,9 +1,11 @@
+import sys
+sys.path.append('/home/wangzihang/FL-DP/')
 from fl_utils.center_average_model_with_weights import set_averaged_weights_as_main_model_weights, \
     set_averaged_weights_as_main_model_weights_fully_averaged
 from fl_utils.local_clients_train_process import local_clients_train_process_without_dp_one_epoch, \
     local_clients_train_process_without_dp_one_batch
 from fl_utils.send_main_model_to_clients import send_main_model_to_clients
-from fed_data_distribution.dirichlet_nonIID_data import fed_dataset_NonIID_Dirichlet
+from data.fed_data_distribution.dirichlet_nonIID_data import fed_dataset_NonIID_Dirichlet
 from fl_utils.optimizier_and_model_distribution import create_model_optimizer_criterion_dict
 from fed_data_distribution.pathological_nonIID_data import pathological_split_noniid
 from data.get_data import get_data
