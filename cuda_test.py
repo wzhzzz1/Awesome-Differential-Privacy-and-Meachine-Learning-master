7a292a5a7a23a6bc3410d63a66a70bca68633967
wangzihang <1013054127@qq.com>
2023-09-11 11:52:48 +0800

    修改文件的保存地址

--- cuda_test.py
+++ cuda_test.py
@@ -1,9 +1,13 @@
 import torch
+
+
 def gpu_is_available():
     print('\nGPU details:')
     print(f'    gpu_is_available      : ', torch.cuda.is_available())
     print(f'    cuda_device_count     : ', torch.cuda.device_count())
     print(f'    cuda_device_name      : ', torch.cuda.get_device_name())
     print(f'    cuda_device_capability: ', torch.cuda.get_device_capability(0))
-                          
+
+
 gpu_is_available()
+#
