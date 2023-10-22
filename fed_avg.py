f7b92700b5babea2e76526db0aeb8287df614b42
wzhzzz1 <132534771+wzhzzz1@users.noreply.github.com>
2023-10-23 01:51:15 +0800

    Update fed_avg.py

--- fed_avg.py
+++ fed_avg.py
@@ -1,12 +1,12 @@
 import sys
 sys.path.append('/home/wangzihang/FL-DP/')
-from FL_DP.fl_utils.center_average_model_with_weights import set_averaged_weights_as_main_model_weights, \
+from FL_and_DP.fl_utils.center_average_model_with_weights import set_averaged_weights_as_main_model_weights, \
     set_averaged_weights_as_main_model_weights_fully_averaged
-from FL-DP.fl_utils.local_clients_train_process import local_clients_train_process_without_dp_one_epoch, \
+from FL_and_DP.fl_utils.local_clients_train_process import local_clients_train_process_without_dp_one_epoch, \
     local_clients_train_process_without_dp_one_batch
-from FL-DP.fl_utils.send_main_model_to_clients import send_main_model_to_clients
+from FL_and_DP.fl_utils.send_main_model_to_clients import send_main_model_to_clients
 from data.fed_data_distribution.dirichlet_nonIID_data import fed_dataset_NonIID_Dirichlet
-from FL-DP.fl_utils.optimizier_and_model_distribution import create_model_optimizer_criterion_dict
+from FL_and_DP.fl_utils.optimizier_and_model_distribution import create_model_optimizer_criterion_dict
 from data.fed_data_distribution.pathological_nonIID_data import pathological_split_noniid
 from data.get_data import get_data
 from model.CNN import CNN, Cifar10CNN
