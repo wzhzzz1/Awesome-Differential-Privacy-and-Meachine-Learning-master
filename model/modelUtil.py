67ec41a516184f444691f43e8f8b695d8705582e
wangzihang <1013054127@qq.com>
2023-10-23 14:32:51 +0800

    添加modelUtil.py文件

---
+++ model/modelUtil.py
@@ -0,0 +1,66 @@
+import torch
+from torchvision.models import alexnet, resnet18
+from torch.nn.functional import relu, softmax, max_pool2d
+from torch.nn.utils import spectral_norm
+from torch import nn, tanh
+import copy
+from opacus.grad_sample import register_grad_sampler
+from typing import Dict
+import torchvision
+from collections import OrderedDict
+from numpy import median
+import numpy as np
+import torch.nn.functional as func
+
+
+
+
+class InputNorm(nn.Module):
+    def __init__(self, num_channel, num_feature):
+        super().__init__()
+        self.num_channel = num_channel
+        self.gamma = nn.Parameter(torch.ones(num_channel))
+        self.beta = nn.Parameter(torch.zeros(num_channel, num_feature, num_feature))
+    def forward(self, x):
+        if self.num_channel == 1:
+            x = self.gamma*x
+            x = x + self.beta
+            return  x
+        if self.num_channel == 3:
+            return torch.einsum('...ijk, i->...ijk', x, self.gamma) + self.beta
+
+class mnist_fully_connected_IN(nn.Module):
+    def __init__(self,num_classes):
+        super(mnist_fully_connected_IN, self).__init__()
+        self.hidden1 = 600
+        self.hidden2 = 100
+        self.fc1 = nn.Linear(28 * 28, self.hidden1, bias=False)
+        self.fc2 = nn.Linear(self.hidden1, self.hidden2, bias=False)
+        self.fc3 = nn.Linear(self.hidden2, num_classes, bias=False)
+        self.relu = nn.ReLU(inplace=False)
+        self.norm = InputNorm(1, 28)
+
+    def forward(self,x):
+        x = self.norm(x)
+        x = x.view(-1, 28 * 28) #将输入变为28*28的一维向量
+        x = relu(self.fc1(x))
+        x = relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class mnist_fully_connected(nn.Module):
+    def __init__(self,num_classes):
+        super(mnist_fully_connected, self).__init__()
+        self.hidden1 = 600
+        self.hidden2 = 100
+        self.fc1 = nn.Linear(28 * 28, self.hidden1, bias=False)
+        self.fc2 = nn.Linear(self.hidden1, self.hidden2, bias=False)
+        self.fc3 = nn.Linear(self.hidden2, num_classes, bias=False)
+        self.relu = nn.ReLU(inplace=False)
+
+    def forward(self,x):
+        x = x.view(-1, 28 * 28)
+        x = relu(self.fc1(x))
+        x = relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
