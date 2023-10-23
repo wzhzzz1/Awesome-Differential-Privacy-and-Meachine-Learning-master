d7f410a3a2fc0ce64b8b39e8037ac6a9865a26d0
wzhzzz1 <132534771+wzhzzz1@users.noreply.github.com>
2023-10-23 12:55:42 +0800

    Update fed_avg.sh

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,1 +1,1 @@
-python fed_avg.py --data='mnist' --client=100 --batchsize=64 --epoch=1 --iters=100 --lr=0.01 --alpha=10 --seed=1 --sr=1
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=100 --lr=0.01 --alpha=10 --seed=1 --sr=1
