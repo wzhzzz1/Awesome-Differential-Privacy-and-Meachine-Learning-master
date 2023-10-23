c33853f673d7e454a197e97e05a1a4b090afc1cc
wangzihang <1013054127@qq.com>
2023-10-23 14:46:26 +0800

    添加modelUtil.py文件

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,1 +1,1 @@
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=100 --lr=0.05 --alpha=10 --seed=1 --sr=1
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=100 --lr=0.1 --alpha=10 --seed=1 --sr=1
