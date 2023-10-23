c528f96d26f5ec2626de309343c70b4f31219c9c
wangzihang <1013054127@qq.com>
2023-10-23 19:34:11 +0800

    添加个性化的部分

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,1 +1,1 @@
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=201 --lr=0.1 --alpha=10 --seed=1 --sr=1 --personal=0
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=201 --lr=0.1 --alpha=10 --seed=1 --sr=1 --personal=1
