3445bc32bde4edf8948418d7b90269af2c5463eb
wangzihang <1013054127@qq.com>
2023-10-24 14:55:07 +0800

    修复bug

--- fed_avg.py
+++ fed_avg.py
@@ -3,7 +3,7 @@ sys.path.append('/home/wangzihang/FL-DP/')
 from FL_and_DP.fl_utils.center_average_model_with_weights import set_averaged_weights_as_main_model_weights, \
     set_averaged_weights_as_main_model_weights_fully_averaged
 from FL_and_DP.fl_utils.local_clients_train_process import local_clients_train_process_without_dp_one_epoch, \
-    local_clients_train_process_without_dp_one_batch
+    local_clients_train_process_without_dp_one_batch,local_clients_train_process_one_epoch_with_ldp_gaussian
 from FL_and_DP.fl_utils.send_main_model_to_clients import send_main_model_to_clients
 from data.fed_data_distribution.dirichlet_nonIID_data import fed_dataset_NonIID_Dirichlet
 from FL_and_DP.fl_utils.optimizier_and_model_distribution import create_model_optimizer_criterion_dict
