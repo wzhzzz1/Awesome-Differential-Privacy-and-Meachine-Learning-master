c5fd9f56c1e25183082a3eef3459053984605df4
wangzihang <1013054127@qq.com>
2023-10-25 19:29:25 +0800

    修改一下参数

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,2 +1,2 @@
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=50 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=8 --personal=1 --ptype='liner' --usedp=1
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=50 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=8 --personal=1 --ptype='unliner' --usedp=1
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=10 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=6 --personal=1 --ptype='liner' --usedp=1
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=10 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=6 --personal=1 --ptype='unliner' --usedp=1
