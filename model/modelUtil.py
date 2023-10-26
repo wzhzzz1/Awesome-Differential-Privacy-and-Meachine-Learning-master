2f995a24d0c753f0d111b4ea72d5d763dfce19b0
wangzihang <1013054127@qq.com>
2023-10-26 21:27:10 +0800

    尝试ax^b+c

--- model/modelUtil.py
+++ model/modelUtil.py
@@ -96,13 +96,11 @@ class InputNorm1(nn.Module):
         super().__init__()
         self.num_channel = num_channel
         self.gamma = nn.Parameter(torch.ones(num_channel))
-        self.gamma1 = nn.Parameter(torch.ones(num_channel))
-        self.eps = nn.Parameter(torch.ones(num_channel))
         self.beta = nn.Parameter(torch.zeros(num_channel, num_feature, num_feature))
 
     def forward(self, x):
         if self.num_channel == 1:
-            x = self.gamma1 * torch.log(1+self.gamma*x)
+            x = self.gamma * x
             x = x + self.beta
             return x
         if self.num_channel == 3:
