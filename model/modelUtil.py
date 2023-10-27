2b7a3758ffca013291a6eb7f7f5fdb1cc9a24ff4
wangzihang <1013054127@qq.com>
2023-10-27 16:11:50 +0800

    添加更复杂的转化

--- model/modelUtil.py
+++ model/modelUtil.py
@@ -95,15 +95,19 @@ class InputNorm1(nn.Module):
     def __init__(self, num_channel, num_feature):
         super().__init__()
         self.num_channel = num_channel
-        self.gamma = nn.Parameter(torch.ones(num_channel))
-        self.gamma1 = nn.Parameter(torch.ones(num_channel))
-        self.beta = nn.Parameter(torch.zeros(num_channel, num_feature, num_feature))
 
+        self.hidden1 = 400
+        self.hidden2 = 100
+        self.fc1 = nn.Linear(28 * 28, self.hidden1, bias=False)
+        self.fc2 = nn.Linear(self.hidden1, self.hidden2, bias=False)
+        self.fc3 = nn.Linear(self.hidden2, 28 * 28, bias=False)
+        self.relu = nn.ReLU(inplace=False)
     def forward(self, x):
         if self.num_channel == 1:
-            x = self.gamma * x
-            x = self.gamma1*torch.log(x+1)
-            x = x + self.beta
+            x = x.view(-1, 28 * 28)
+            x = relu(self.fc1(x))
+            x = relu(self.fc2(x))
+            x = self.fc3(x)
             return x
         if self.num_channel == 3:
             return torch.einsum('...ijk, i->...ijk', x, self.gamma) + self.beta
