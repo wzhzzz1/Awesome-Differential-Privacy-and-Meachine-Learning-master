bdacd82efe945864647841467094f47a2936ba1d
wangzihang <1013054127@qq.com>
2023-11-12 20:35:43 +0800

    输出非DP下每轮每个客户端的训练损失

--- train_and_validation/train.py
+++ train_and_validation/train.py
@@ -20,6 +20,7 @@ def train(model, train_loader, optimizer):
     train_loss = 0.0
     train_acc= 0.0
     i=0
+    num_examples = 0
 
     for id, (data, target) in enumerate(train_loader):
         # if id==0:
@@ -30,7 +31,33 @@ def train(model, train_loader, optimizer):
         optimizer.zero_grad()  # 梯度清空
         loss.backward()  # 梯度求导
         optimizer.step()  # 参数优化更新
+        train_loss += loss.item()
+        num_examples += data.size(0)  # 记录样本数
+
+    avg_loss = train_loss / num_examples
+    return avg_loss,train_acc
+'''
+    model.eval()
+    num_examples = 0
+    test_loss = 0
+    correct = 0
+    device='cpu'
+    model.to(device)
+    with torch.no_grad():
+        for id,(data, target) in enumerate(test_loader):
+            # if id==0:
+            #     print("测试集：",data[0]) #这边同样DPSGD的验证集也是浮点型的
+            data, target = data.to(device), target.to(device)
+            output = model(data.to(torch.float32))
+            test_loss += F.cross_entropy(output, target.to(torch.long), reduction='sum').item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            num_examples += len(data)
+
+    test_loss /= num_examples
+    test_acc = 100. * correct / num_examples
 
 
 
-    return train_loss,train_acc
+    return test_loss, test_acc
+'''
