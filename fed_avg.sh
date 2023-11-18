a63898ad58bf0e7705686de637f11f83deb20d20
wangzihang <1013054127@qq.com>
2023-11-18 16:50:51 +0800

    绷不住了

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,1 +1,1 @@
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=50 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=8 --personal=1 --ptype='liner' --usedp=1
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=3 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=8 --personal=1 --ptype='liner' --usedp=1
