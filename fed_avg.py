cdeb591f602b90bad76953e3d99be394744519cc
wangzihang <1013054127@qq.com>
2023-11-18 16:13:18 +0800

    绷不住了

--- fed_avg.py
+++ fed_avg.py
@@ -102,11 +102,11 @@ def fed_avg(train_data, test_data, number_of_clients, learning_rate, momentum, n
         if i==iters-1:
             print('train finish')
             with torch.no_grad():
-                for i in range(len(clients_model_list)):
-                    if i ==9:
-                        for key, value in clients_model_list[i].state_dict().items():
+                for j in range(len(clients_model_list)):
+                    if j ==9:
+                        for key, value in clients_model_list[j].state_dict().items():
                             # if 'norm'  in key or 'bn' in key or 'downsample.1' in key:  # 这个downsample是resnet里特有的，norm就是个性化层
-                            print(i)
+                            print(j)
                             print(key)
                             print(value)
                 print('global model')
@@ -119,12 +119,12 @@ def fed_avg(train_data, test_data, number_of_clients, learning_rate, momentum, n
         if i==iters-1:
             print('fuhe')
             with torch.no_grad():
-                for i in range(len(clients_model_list)):
-                    if i ==9:
+                for j in range(len(clients_model_list)):
+                    if j ==9:
                         print('local model')
                         for key, value in clients_model_list[i].state_dict().items():
                             # if 'norm'  in key or 'bn' in key or 'downsample.1' in key:  # 这个downsample是resnet里特有的，norm就是个性化层
-                            print(i)
+                            print(j)
                             print(key)
                             print(value)
         '''
