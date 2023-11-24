42981fde33c196efb03d036807ac6ef56040565a
wangzihang <1013054127@qq.com>
2023-11-24 22:58:19 +0800

    修复个性化模型错误的bug

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,1 +1,1 @@
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=50 --lr=0.1 --alpha=0.05 --seed=1 --sr=1 --eps=8 --personal=1 --ptype='liner' --usedp=1
+python fed_avg.py --data='cifar10' --client=10 --batchsize=64 --epoch=1 --iters=50 --lr=0.1 --alpha=1 --seed=1 --sr=1 --eps=8 --personal=0 --ptype='liner' --usedp=0
