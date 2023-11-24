4e007a3a58605130caf14533ffac1ce1b69d430c
wangzihang <1013054127@qq.com>
2023-11-24 15:24:54 +0800

    添加个性化模型resnet18_IN和resnet18_IN1

--- model/modelUtil.py
+++ model/modelUtil.py
@@ -310,7 +310,86 @@ class ResNet(nn.Module):
         out = self.fc(out)
         return out
 
+class ResNet_IN(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=10):
+        super(ResNet_IN, self).__init__()
+        self.norm = InputNorm1(3, 32)
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)   #strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.norm(x)
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+class ResNet_IN1(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=10):
+        super(ResNet_IN1, self).__init__()
+        self.norm = InputNorm1(3, 32)
+        self.bn = OutputNorm1(1, 10)
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)   #strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.norm(x)
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        out = self.bn(out)
+        return out
 
 def ResNet18():
 
     return ResNet(ResidualBlock)
+
+def ResNet18_IN():
+    return ResNet_IN(ResidualBlock)
+
+def ResNet18_IN1():
+    return ResNet_IN1(ResidualBlock)
