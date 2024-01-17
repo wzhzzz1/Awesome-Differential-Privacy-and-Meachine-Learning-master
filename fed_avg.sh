2b26f4540074e41765856033cc0fb706b606df6a
wangzihang <1013054127@qq.com>
2024-01-17 16:15:51 +0800

    添加从argparse选择相应模型的部分

--- fed_avg.sh
+++ fed_avg.sh
@@ -1,1 +1,1 @@
-python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=50 --lr=0.1 --alpha=100 --seed=1 --sr=1 --eps=8 --personal=0 --ptype='double' --usedp=0
+python fed_avg.py --data='mnist' --client=10 --batchsize=64 --epoch=1 --iters=50 --model='DNN' --lr=0.1 --alpha=100 --seed=1 --sr=1 --eps=8 --personal=0 --ptype='double' --usedp=0
