cd60307bc996f99222fe1075a5a32dde0647b8d6
Your Name <you@example.com>
2024-02-21 23:52:58 +0800

    添加全参数解耦

--- FL_and_DP/fl_utils/send_main_model_to_clients.py
+++ FL_and_DP/fl_utils/send_main_model_to_clients.py
@@ -7,7 +7,7 @@ def send_main_model_to_clients(center_model, clients_model_list):
         weights=center_model.state_dict()
         for i in range(len(clients_model_list)):
             for key, value in clients_model_list[i].state_dict().items():
-                if 'norm' not in key and 'bn' not in key and 'downsample.1' not in key:  # 这个downsample是resnet里特有的，norm就是个性化层
+                if 'norm' not in key and 'bn' not in key and 'weight' not in key and 'downsample.1' not in key:  # 这个downsample是resnet里特有的，norm就是个性化层
                     clients_model_list[i].state_dict()[key].data.copy_(weights[key])
     return clients_model_list
 
